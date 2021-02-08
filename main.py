e2557c5ece78614bf2d935ad839f8e563f0bb538
Brandon Braner <brandonbraner@oneome.com>
2021-02-08 17:47:59 -0600

    inital file compare

---
+++ main.py
@@ -0,0 +1,58 @@
+import filecmp
+import io
+
+file_one = 'xml1.xml'
+file_two = 'xml2.xml'
+
+
+def get_file_end(file_reader):
+    file_reader.seek(0, io.SEEK_END)  # go to the file end.
+    file_reader_eof = file_reader.tell()
+    file_reader.seek(0, 0)  # go to the start of the file
+    return file_reader_eof
+
+
+files_are_same = filecmp.cmp(file_one, file_two)
+if files_are_same:
+    print(f'Files {file_one} and {file_two} are the same')
+    exit(code=0)
+
+file_reader_1 = open(file_one, "r")
+file_reader_1_eof = get_file_end(file_reader_1) 
+
+file_reader_2 = open(file_two, "r")
+file_reader_2_eof = get_file_end(file_reader_2) 
+
+file_1_is_at_end = False
+file_2_is_at_end = False
+line_number = 0
+
+while True:
+
+    if file_reader_1.tell() == file_reader_1_eof:
+        file_1_is_at_end = True
+
+    if file_reader_2.tell() == file_reader_2_eof:
+        file_2_is_at_end = True
+
+    if file_1_is_at_end and file_2_is_at_end:
+        print("Both files are at the end, they are the same")
+    elif file_1_is_at_end and not file_2_is_at_end:
+        print(f"{file_one} reached the end before {file_two}. They are not the same")
+    elif not file_1_is_at_end and file_2_is_at_end:
+        print(f"{file_two} reached the end before {file_one}. They are not the same")
+
+    file_one_string = file_reader_1.readline()
+    file_two_string = file_reader_2.readline()
+
+    if file_one_string == file_two_string:
+        line_number = line_number + 1
+        continue
+    else:
+        print(f"Files are different at line number {line_number}")
+        print(f"{file_one} is {file_one_string}")
+        print(f"{file_two} is {file_two_string}")
+        break
+
+
+
